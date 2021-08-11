b7ccd94ffc027161dabf3e74c1bd633664b74e44
Andrei <andrei@0xab.com>
2021-08-11 14:28:51 -0400

    API docs

--- server/Machinations/Api.hs
+++ server/Machinations/Api.hs
@@ -12,35 +12,61 @@ import Servant.OpenApi
 import Servant.Swagger.UI
 import Servant.Swagger.UI.Core
 import Control.Lens
+import Machinations.Misc
 
-instance ToSchema StateEdgeModifiers
-instance ToSchema StateFormula
-instance ToSchema Limits
-instance ToSchema Formula
-instance ToSchema Waiting
-instance ToSchema AnyLabel
-instance ToSchema TransferType
-instance ToSchema DistributionType
-instance ToSchema PushAction
-instance ToSchema Interval
-instance ToSchema Overflow
-instance ToSchema ResourceFormula
-instance ToSchema PushPullAction
-instance ToSchema StateEdge
-instance ToSchema PullAction
-instance ToSchema ResourceEdge
-instance ToSchema NodeActivation
-instance ToSchema NodeType
-instance ToSchema Resource
-instance ToSchema Node
-instance ToSchema Graph
+instance ToSchema StateFormula where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema Limits where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions (prefixOptions "limits"))
+instance ToSchema Formula where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema Waiting where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions (prefixOptions "_waiting"))
+instance ToSchema AnyLabel where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptionsSingle)
+instance ToSchema TransferType where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema DistributionType where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions (prefixOptions "deterministic"))
+instance ToSchema PushAction where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema Interval where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema Overflow where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema ResourceFormula where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema PushPullAction where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema StateEdge where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema PullAction where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema ResourceEdge where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema NodeActivation where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema NodeType where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema Resource where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions (prefixOptions "resource"))
+instance ToSchema Node where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema Graph where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions (prefixOptions "graph"))
+instance ToSchema StateEdgeModifiers where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
 deriving newtype instance ToSchema NodeLabel
 deriving newtype instance ToSchema ResourceEdgeLabel
 deriving newtype instance ToSchema StateEdgeLabel
-instance ToSchema Condition
-instance ToSchema Machination
-instance ToSchema RunMachination
-instance ToSchema RunResult
+instance ToSchema Condition where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions mjsonOptions)
+instance ToSchema Machination where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions (prefixOptions "Machination"))
+instance ToSchema RunMachination where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions (prefixOptions "runMachination"))
+instance ToSchema RunResult where
+  declareNamedSchema = genericDeclareNamedSchema (fromAesonOptions (prefixOptions "runResult"))
 
 type RawAPI = "api" :> (("test"  :> Get '[JSON] Text)
                          :<|> ("render" :> ReqBody '[JSON] Machination :> Post '[JSON] Text )
